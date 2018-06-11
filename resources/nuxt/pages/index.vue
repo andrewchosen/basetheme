917db3b02d298f5f888c8b622cdfd444c2691c16
Andrew Lancaster <andrewlancaster@Andrews-MacBook-Pro.local>
2018-06-10 21:55:07 -0500

    Add NuxtJS with example

---
+++ resources/nuxt/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Hello {{ name }}! Hello again. And again!</h1>
+</template>
+
+<style>
+    body {
+        background: #212121;
+        color: #f5f5f5;
+        padding: 6rem;
+    }
+    h1 {
+        text-align: center;
+    }
+</style>
+
+<script>
+    export default {
+        data: () => {
+            return { name: 'world' };
+        },
+    };
+</script>
